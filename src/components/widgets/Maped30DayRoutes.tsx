d2861424bfc35d7f91e72e05d574de9f3e6bb87a
Miguel Chavez <toonchavez8@gmail.com>
2023-09-08 22:47:53 -0600

    adding routes and styling Maped30DayRoutes

--- src/components/widgets/Maped30DayRoutes.tsx
+++ src/components/widgets/Maped30DayRoutes.tsx
@@ -9,26 +9,31 @@ export default function Maped30DayRoutes() {
 	}
 
 	return (
-		<ul className="p-2 z-10">
-			{dayLinks.map((link: { page: string }) => {
-				const pageName = String(link.page).slice(0, -6); // Convert link to a string and then slice the last 6 characters (.astro)
-				const isDone = isPageDone(pageName); // Check if the page is done
-
-				// Determine the class based on whether the page is done or not
-				const linkClass = isDone ? "" : "btn-disabled";
-
-				return (
-					<li key={pageName}>
-						<a
-							href={`/day/${pageName}`}
-							className={`block ${linkClass} cursor-pointer`}
-							// Add other tailwind classes or styles as needed
-						>
-							{pageName}
-						</a>
-					</li>
-				);
-			})}
+		<ul className="p-2 z-10 scroll-smooth snap-y snap-start menu ">
+			<li>
+				<a className="menu-title  " href="/30days">
+					30 Days
+				</a>
+				<ul>
+					{dayLinks.map((link: { page: string }) => {
+						const pageName = String(link.page).slice(0, -6); // Convert link to a string and then slice the last 6 characters (.astro)
+						const isDone = isPageDone(pageName); // Check if the page is done
+						// Determine the class based on whether the page is done or not
+						const linkClass = isDone ? "" : "btn-disabled";
+						return (
+							<li key={pageName}>
+								<a
+									href={`/day/${pageName}`}
+									className={`block ${linkClass} cursor-pointer`}
+									// Add other tailwind classes or styles as needed
+								>
+									{pageName}
+								</a>
+							</li>
+						);
+					})}
+				</ul>
+			</li>
 		</ul>
 	);
 }
